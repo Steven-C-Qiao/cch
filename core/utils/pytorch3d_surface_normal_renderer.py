dfed5874a1507944ca82068d8e71bceed4cbb49f
Steven-C-Qiao <cq244@cam.ac.uk>
2025-05-05 15:58:56 +0100

    cleanup renderer comments

--- core/utils/pytorch3d_surface_normal_renderer.py
+++ core/utils/pytorch3d_surface_normal_renderer.py
@@ -7,7 +7,6 @@ from einops import rearrange
 
 from pytorch3d.structures import Meshes
 from pytorch3d.renderer import (
-    look_at_view_transform,
     FoVPerspectiveCameras,
     RasterizationSettings,
     MeshRenderer,
@@ -41,31 +40,19 @@ class SurfaceNormalRenderer(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
-        self.renderer = self.create_normal_renderer()
-
-    def create_normal_renderer(self):
-        # Create a renderer for surface normals
-        # R, T = look_at_view_transform(dist=2., elev=-10, azim=0, degrees=True)
-        # R, T = look_at_view_transform(at=torch.tensor([0, 0, 2.]), degrees=True)
-        # cameras = FoVPerspectiveCameras(R=R, T=T)
-        
-        # Create rasterization settings
         raster_settings = RasterizationSettings(
             image_size=512,
             blur_radius=0.0,
             faces_per_pixel=1,
         )
         
-        # Create a renderer
-        renderer = MeshRenderer(
+        self.renderer = MeshRenderer(
             rasterizer=MeshRasterizer(
-                # cameras=cameras,
                 raster_settings=raster_settings
             ),
             shader=SimpleShader()
         )
-        
-        return renderer
+    
 
     def forward(self, vertices, faces=None, R=None, T=None):
         """
@@ -121,9 +108,6 @@ class SurfaceNormalRenderer(pl.LightningModule):
         # discard alpha channel
         normals = images[..., :3].cpu().numpy()
 
-        # Normalize the normals
-        # normals = normals / (np.linalg.norm(normals, axis=3, keepdims=True) + 1e-6)
-        
         return rearrange(normals, '(b n) c h w -> b n c h w', b=B, n=N)
 
 if __name__ == "__main__":
@@ -135,37 +119,21 @@ if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     num_angles = 6
 
-    # smpl_model = smplx.create(
-    #     model_path='model_files',
-    #     model_type='smpl',
-    #     gender='neutral',
-    #     device=device
-    # )
-    # smpl_faces = smpl_model.faces
-
 
     data = np.load(CAPE_PATH)
-    # for key, value in data.items():
-    #     print(key, value.shape)
-    # print(data['transl'])
 
     vertices = torch.tensor(data['v_posed'], dtype=torch.float32)
-    # faces = torch.tensor(smpl_faces, dtype=torch.int32)
-
 
     at = torch.tensor(data['transl']).float()[None]
     at[:, 1] -= 0.2    
     R, T = sample_cameras(1, num_angles, at)
 
 
-
     renderer = SurfaceNormalRenderer()
 
     normals = renderer(vertices[None], 
-                            #    faces, 
                                R=R, 
                                T=T)
-    # print(normals.shape)
 
     n_subplots = normals.shape[1]
     fig, axs = plt.subplots(1, n_subplots, figsize = (n_subplots * 4, 4))
@@ -173,18 +141,3 @@ if __name__ == "__main__":
         axs[i].imshow(normals[0, i])
     plt.savefig('tinkering/test_normal_renderer.png')
     plt.show()
-
-
-
-    # fig, axs = plt.subplots(1, num_angles, figsize = (num_angles * 4, 4))
-    # for i, angle in enumerate(np.linspace(0, 360, num_angles)):
-    #     R, T = look_at_view_transform(dist=2., elev=0, at=at, azim=angle, degrees=True)
-    #     cameras = FoVPerspectiveCameras(R=R, T=T, device=device)
-            
-    #     normals = renderer.forward(vertices, faces, cameras)
-    #     axs[i].imshow(normals)
-
-    # # Save before showing
-    # plt.savefig('tinkering/surface_normals_obj_coord.png')
-    # plt.show()
-    # plt.close()
